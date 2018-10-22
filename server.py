47e156e88fe0a95025fec735b9e54f230c2a20b5
Evgeny Cherkashin <eugeneai@irnok.net>
2018-10-22 08:49:32 +0800

    Added the submodule of site template

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+import os
+import os.path
 
 TEMPLATE = \
-"""
+    """
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
@@ -19,17 +21,36 @@ TEMPLATE = \
 </html>
 """
 
+TDIR = os.getcwd()
+
+
+def t(template):
+    return os.path.join(TDIR, "templates", template)
+
 
 def hello_world(request):
-    S = TEMPLATE.replace("[name]", request.matchdict["name"])
-    return Response(S)
+    return {"name": request.matchdict["name"]}
+
+
+def calc(request):
+    print(request.POST)
+    return "Ok"
+
 
 if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('hello', '/hello/{name}')
-        config.add_view(hello_world, route_name='hello')
+        config.add_view(hello_world,
+                        route_name='hello',
+                        renderer=t('index.pt'))
+        config.include('pyramid_chameleon')
+        config.add_static_view(name='vendors', path=t('vendors'))
+        config.add_static_view(name='build', path=t('build'))
+
+        config.add_route('calc', '/calc')
+        config.add_view(calc, route_name='calc')
+
         app = config.make_wsgi_app()
-    server = make_server('127.0.0.1', 9080, app)
+    server = make_server('0.0.0.0', 8080, app)
     print("Serving....")
     server.serve_forever()
-
